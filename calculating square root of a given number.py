08f0cbfb99af3762a8751489529095f565a8faf9
majjitejeswani <108048290+majjitejeswani@users.noreply.github.com>
2022-06-24 09:33:47 +0530

    calculating square root of a given number

---
+++ calculating square root of a given number.py
@@ -0,0 +1,5 @@
+#python program to find square root of a given number
+import math
+num = float(input("Enter a number:"))
+square_root = math.sqrt(num)
+print("Square root of %d is:" %num,square_root)
